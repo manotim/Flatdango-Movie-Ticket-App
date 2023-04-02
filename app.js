1aafe2cb2edc646bb82bc0a7a1aa11128b076b5e
manoti <manotimike@gmail.com>
2023-04-02 17:13:28 +0300

    added delete item to films list

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-const moviePlaceHolder = () => {
+const moviePlace = () => {
 
 fetch("http://localhost:3000/films")
 .then(res => res.json())
@@ -9,19 +9,19 @@ fetch("http://localhost:3000/films")
     const firstMovie = data[0]
     // console.log(firstMovie)
 
-    const filmImg = document.getElementById("poster")
-    const movieTitle = document.getElementById("filmTitle")
-    const movieDescr = document.getElementById("movieDescription")
-    const runningTime = document.getElementById("runtime")
-    const showingTime = document.getElementById("showtime")
-    const availTicket =document.getElementById(`ticketsAvailable${data.id}`)
+    const movieImg = document.getElementById("poster")
+    const movieName = document.getElementById("filmTitle")
+    const movieDes = document.getElementById("movieDescription")
+    const runTm = document.getElementById("runtime")
+    const showTm = document.getElementById("showtime")
+    const availableTckt =document.getElementById(`ticketsAvailable${data.id}`)
 
-    filmImg.src = firstMovie.poster
-    movieTitle.innerText = firstMovie.title
-    movieDescr.innerText = firstMovie.description
-    runningTime.innerText =`Runtime: ${firstMovie.runtime} minutes`
-    showingTime.innerText =`Showtime: ${firstMovie.showtime}`
-    availTicket.innerText =`Tickets Available: (${firstMovie.capacity - firstMovie.tickets_sold})`
+    movieImg.src = firstMovie.poster
+    movieName.innerText = firstMovie.title
+    movieDes.innerText = firstMovie.description
+    runTm.innerText =`Runtime: ${firstMovie.runtime} minutes`
+    showTm.innerText =`Showtime: ${firstMovie.showtime}`
+    availableTckt.innerText =`Tickets Available: (${firstMovie.capacity - firstMovie.tickets_sold})`
 
     const ticketBuy = document.getElementById(`buyTicket${data.id}`)
     let tickets = Number(firstMovie.capacity - firstMovie.tickets_sold)
@@ -34,9 +34,9 @@ fetch("http://localhost:3000/films")
             const frstMovie = document.getElementById("1")
             frstMovie.innerHTML=`${firstMovie.title}  <span class="badge bg-danger">SOLD OUT</span>`
 
-            availTicket.innerHTML = `Ticketd available:  <span class="badge bg-danger">SOLD OUT</span>`
+            availableTckt.innerHTML = `Ticketd available:  <span class="badge bg-danger">SOLD OUT</span>`
         }else{
-            availTicket.innerText = `Tickets available: (${tickets})`
+            availableTckt.innerText = `Tickets available: (${tickets})`
         }
 
         // fetch function to persist Tickets sold in the server
@@ -52,7 +52,7 @@ fetch("http://localhost:3000/films")
 })
 }
 
-const movieList = () => {
+const movieItem = () => {
 
     fetch("http://localhost:3000/films")
     .then(res => res.json())
@@ -61,21 +61,27 @@ const movieList = () => {
         const filmData = data
 
         for(let i = 0; i < filmData.length; i++){
-            let item = filmData[i]
-            // console.log(item)
-            const movieList = document.createElement("li")
+            let movie = filmData[i]
+            // console.log(movie)
+            const movieItem = document.createElement("li")
             const list = document.getElementById("showingMovie")
 
-            movieList.classList.add("border")
+            movieItem.classList.add("border")
 
-            movieList.setAttribute('id',`${item.id}`)
+            movieItem.setAttribute('id',`${movie.id}`)
 
-            movieList.innerText = item.title
-            console.log(item.title)
+            movieItem.innerText = movie.title
+            console.log(movie.title)
 
-            list.appendChild(movieList)
+            list.appendChild(movieItem)
 
-            movieList.addEventListener('click',()=>{
+            const deleteBtn = document.createElement("button")
+            deleteBtn.classList.add("btn", "btn-danger")
+            deleteBtn.setAttribute('id',`${movie.id}-delete`)
+            deleteBtn.innerText = "Delete"
+            movieItem.appendChild(deleteBtn)
+
+            movieItem.addEventListener('click',()=>{
                 const filmImage = document.getElementById("poster")
                 const filmTitle = document.getElementById("filmTitle")
                 const filmDescr = document.getElementById("movieDescription")
@@ -84,22 +90,22 @@ const movieList = () => {
                 const availTickets =document.getElementById("ticketsAvailable")
 
 
-                filmImage.src = item.poster
-                filmTitle.innerText = item.title
-                filmDescr.innerText = item.description
-                runTime.innerHTML =`Runtime:<span>${item.runtime}</span>`
-                showTime.innerText =`Showtime: ${item.showtime}`
-                availTickets.innerText =`Tickets available: (${item.capacity - item.tickets_sold})`
+                filmImage.src = movie.poster
+                filmTitle.innerText = movie.title
+                filmDescr.innerText = movie.description
+                runTime.innerHTML =`Runtime:<span>${movie.runtime}</span>`
+                showTime.innerText =`Showtime: ${movie.showtime}`
+                availTickets.innerText =`Tickets available: (${movie.capacity - movie.tickets_sold})`
 
                 const ticketsBuy = document.getElementById("buyTicket")
-                let ticket = Number(item.capacity - item.tickets_sold)
+                let ticket = Number(movie.capacity - movie.tickets_sold)
 
                 ticketsBuy.addEventListener('click',()=>{
 
                     
                     ticket --
                     if(ticket <= 0){
-                        movieList.innerHTML =`${item.title} <span class="badge bg-danger">SOLD OUT</span>`
+                        movieItem.innerHTML =`${movie.title} <span class="badge bg-danger">SOLD OUT</span>`
 
                         availTickets.innerHTML = `Tickets available: <span class="badge bg-danger">SOLD OUT</span>`
 
@@ -127,6 +133,6 @@ const movieList = () => {
 
 
 
-moviePlaceHolder()
-movieList()
+moviePlace()
+movieItem()
 })
